cc9ea060ad258d48a5fa478497d0179558634956
Naycon <johanopersson@gmail.com>
2025-10-24 14:51:37 +0000

    Deploying to gh-pages from @ backstage/backstage@5c614fff85d7844b6662f7f5751eb3ec5d5ae842 🚀

--- storybook/assets/get-DdhrmVcN.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./iframe-CVxNCVyG.js";import{r as t}from"./appWrappers-CoNp-TeF.js";var o=t();const a=r(o);export{a as g};
--- storybook/assets/useFormReset-v22PEJRc.js
+++
@@ -1,1 +0,0 @@
-import{c as i}from"./utils-DiAkVa2G.js";import{r as n}from"./iframe-CVxNCVyG.js";function c(r,a,d){let t=i(()=>{d&&d(a)});n.useEffect(()=>{var o;let e=r==null||(o=r.current)===null||o===void 0?void 0:o.form;return e?.addEventListener("reset",t),()=>{e?.removeEventListener("reset",t)}},[r,t])}export{c as $};
